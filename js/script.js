42e8c6c00aa7c5ab1b3fb0b64df3651cdf4284da
Marjib Chiran <chiranmarjib88@gmail.com>
2023-09-02 13:54:21 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// text change
+
+function textChange(){
+    document.getElementById('h2') .innerHTML="Hello Marjib Chiran";
+}
+
+function textChange2(){
+    document.getElementById('h2-2') .innerHTML="This is Marjib Chiran";
+}
+function textChange3(){
+    document.getElementById('h2-3') .innerHTML="Yes, I am Marjib Chiran";
+}
+
+function textChange4(){
+    document.getElementById('h2-4') .innerHTML="I am goind good and you?";
+}
+function textChange6(){
+    document.getElementById('h2-6') .innerHTML="What are you doing now bro?";
+}
+
+function textChange7(){
+    document.getElementById('h2-7') .innerHTML="What are you doing now bro?";
+}
